4333bb2b5a094b275bb078bb8841043276401174
maobonhaha <maobonhaha@gmail.com>
2025-01-11 11:56:51 +0800

    。。

--- sample.py
+++ sample.py
@@ -7,11 +7,9 @@ import numpy as np
 import tensorflow as tf
 from PIL import Image
 
-
 os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
 
 WINDOW_NAME = "Digits"
-
 
 def predict_local_image(image_path):
     """预测本地图像中的数字"""
@@ -19,8 +17,11 @@ def predict_local_image(image_path):
     # mnist_model_bp_v1.keras
     # mnist_model_optimized_v9.keras
     # 加载模型
-    loaded_model = tf.keras.models.load_model("mnist_model_mlp.keras")
-
+    try:
+        loaded_model = tf.keras.models.load_model("mnist_model_optimized_v9.keras")
+    except Exception as e:
+        print(f"Error loading model: {e}")
+        return
 
     # 读取图片
     try:
@@ -30,19 +31,27 @@ def predict_local_image(image_path):
         return
 
     # 调整大小为28x28
-    img_data = Image.fromarray(img_data).resize((28, 28))
-    img_data = np.array(img_data)
+    try:
+        img_data = Image.fromarray(img_data).resize((28, 28))
+        img_data = np.array(img_data)
+    except Exception as e:
+        print(f"Error processing image: {e}")
+        return
+
     # 反转颜色（如果是白底黑字）
     img_data = 255 - img_data
     # 归一化处理
     img_data = img_data.astype("float32") / 255.0
     # 增加批次维度
     img_data = np.expand_dims(img_data, axis=0)
+    img_data = np.expand_dims(img_data, axis=-1)  # 增加通道维度
     # 预测
-    prediction = loaded_model.predict(img_data)
-    predicted_digit = np.argmax(prediction)
-    print(f"== RESULT:Predicted digit for the image: {predicted_digit}")
-
+    try:
+        prediction = loaded_model.predict(img_data)
+        predicted_digit = np.argmax(prediction)
+        print(f"== RESULT: Predicted digit for the image: {predicted_digit}")
+    except Exception as e:
+        print(f"Error during prediction: {e}")
 
 def draw(event, x, y, flags, param):
     global img, pre_pts
@@ -64,14 +73,12 @@ def draw(event, x, y, flags, param):
         if cv.imwrite("image.png", img):
             predict_local_image("image.png")
 
-
 def restart():
     """重新初始化窗口"""
     global img, original_img
     img = original_img.copy()  # 重置为初始图像
     cv.imshow(WINDOW_NAME, img)  # 重新显示图像
     cv.setMouseCallback(WINDOW_NAME, draw)  # 设置鼠标回调
-
 
 def main():
     global img, original_img
@@ -99,7 +106,6 @@ def main():
         elif key == ord("r"):  # 按 'r' 键重置绘制
             print("Key 'r' detected! Resetting...")
             restart()  # 调用重置函数
-
 
 if __name__ == "__main__":
     # 全局变量
